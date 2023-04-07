da9994d87a17256fc4239b4ca5a0e039e913ba4b
Florian Bindereif <Florian.bindereif@gmail.com>
2023-04-07 18:02:17 +0200

    updated: First Final Version for Testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include "../inc/Server.hpp"
 
-int main(void)
+int main(int argc, char** argv)
 {
-	// if (argc < 3)
-	// {
-	// 	std::cout << "Too few arguments: ./irc [port > 0] [password]" << std::endl;
-	// 	return 0;
-	// }
-	// int port = atoi(argv[1]);
-	// if (port <= 0)
-	// {
-	// 	std::cout << "bad port: ./irc [port > 0] [password]" << std::endl;
-	// 	return 0;
-	// }
+	if (argc < 3)
+	{
+		std::cout << "Too few arguments: ./irc [port > 0] [password]" << std::endl;
+		return 0;
+	}
+	int port = atoi(argv[1]);
+	if (port <= 0)
+	{
+		std::cout << "bad port: ./irc [port > 0] [password]" << std::endl;
+		return 0;
+	}
 	try
 	{
 		irc::Server server;
-		server.Init();
+		server.Init(argv[2], port);
 		while(true)
 		{
 			server.Run();
